ff142f3e3e537e2ce1acfcd873be604b8ffab09c
Laina Cazzell <laina.mater@gmail.com>
2018-02-25 18:01:01 -0800

    Add parameter for fallback install script path

--- jsreport/jsreport.Embedded/jsreport/install.cmd
+++ jsreport/jsreport.Embedded/jsreport/install.cmd
@@ -1,9 +1,17 @@
 REM This is being run by the nuget install and also if you run update.cmd, there should be no need to run it manually otherwise
+set installScriptPath=%~1
 
 if exist "install.js" (
-    "../.bin/node.cmd" install.js    
-) else (
-    "../.bin/node.cmd" "../../packages/jsreport.Embedded.1.0.3/tools/install.js"
+    "../.bin/node.cmd" install.js   
+    exit /b
 )
 
+if "%installScriptPath%" == "" (
+    exit /b 87
+)
 
+if not exist "%installScriptPath%" (
+    exit /b 2
+)
+
+"../.bin/node.cmd" "%installScriptPath%"
